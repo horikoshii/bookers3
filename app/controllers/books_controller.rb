ceba5bd1222e2d6b1c06d7c203e641eb62fa1a68
horikoshii <achiga9696@gmail.com>
2022-12-14 06:19:33 +0000

    AtoZ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id), notice: "You have create book successfully."
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have create book successfully."
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def show
